ea91cc117f9e8d11dc4ce8ed16c73048f2d5394d
logapsystems <logapsystems@gmail.com>
2025-06-03 21:34:42 +0200

    Add project files.

---
+++ open_civilization/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace open_civilization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
